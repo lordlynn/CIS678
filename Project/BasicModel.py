77d2dc4330d076783c89247853c35e5b40cb984d
MSI\zacly <zaclynn26@gmail.com>
2024-03-12 16:42:57 -0400

    Changed isolation forest to support vector classifier

--- Project/BasicModel.py
+++ Project/BasicModel.py
@@ -1,7 +1,6 @@
 import prepareDataSet
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, IsolationForest
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 import matplotlib.pyplot as plt
@@ -12,7 +11,6 @@ trainingImages, trainingTruth, testImages, testTruth = prepareDataSet.getDataset
 # Ideas for improvements: 
 #   1.) Normalize the images (make sure all images use full 0-255 range for brightness)
 #   2.) Can use non destructive transforms to augment data 
-
 
 
 # Create a Random Forest classifier (approaches 0.5 random chance??)
@@ -41,20 +39,18 @@ plt.title("Random Forest Test Accuracy")
 
 
 
-# Create an IsolationForest model
-clf = IsolationForest(contamination=0.5, random_state=42)
+# Create an Support vector classifier model
+clf = SVC(max_iter=1000, tol=0.001, cache_size=1000)
 
 # Train the IsolationForest
-clf.fit(trainingImages)
+clf.fit(trainingImages, trainingTruth)
 
 # Make predictions on the test set
 y_pred = clf.predict(testImages)
 
-# Convert the predictions (-1 for outliers, 1 for inliers) to binary labels (0 or 1)
-y_pred = (y_pred + 1) // 2
-
+# Evaluate the accuracy
 accuracy = accuracy_score(testTruth, y_pred)
-print(f'Isolation Forest Test Accuracy: {accuracy}')
+print(f'SVC Test Accuracy: {accuracy}')
 
 plt.figure()
 plt.scatter(x, y_pred)
@@ -64,7 +60,7 @@ plt.xlabel("Sample (n)")
 plt.ylabel("Classification (0 - normal, 1 - abnormal)")
 plt.yticks([])
 plt.ylim([-0.5, 1.5])
-plt.title("Isolation Forest Test Accuracy")
+plt.title("SVC Test Accuracy")
 
 plt.show()
 
